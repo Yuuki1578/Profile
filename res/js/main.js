d12f335f6e033e8f37f8ff0f6593b139d38fe436
Yuuki1578 <destuawang@gmail.com>
2024-07-10 06:18:29 +0700

    Small changes

--- res/js/main.js
+++ res/js/main.js
@@ -1,10 +1,5 @@
 "use strict";
 
-let button = document.querySelector("button");
-let canvas = document.querySelector("div");
-let digit = 0;
-
-button.addEventListener("click", () => {
-  canvas.innerHTML = `${digit}`;
-  digit += 1;
+$("button").on("click", () => {
+  alert("Hello!");
 })
